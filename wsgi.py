ac6af7988f3a36b3282a5199d00cb1c4af619419
Andrej Smigala <asmigala@redhat.com>
2018-11-16 15:42:22 +0100

    Return JSON

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,11 @@
+import json
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type','text/plain'),('Syndesis-Test', 'openshift-pong-py')])
-    resp = []
+    resp = {}
     for k, v in environ.items():
         if k.startswith("HTTP_"):
             print(k, ":", v)
-            resp.append("{}: {}\n".format(k[5:], v))
-    resp.append(environ["QUERY_STRING"])
-    return [bytes(v, 'ascii') for v in resp]
+            resp[k] = v
+    resp["QUERY_STRING"] = environ["QUERY_STRING"]
+    return [bytes(json.dumps(resp), 'ascii')]
